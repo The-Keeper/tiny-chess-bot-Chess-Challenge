bb9d86ce4cb249ea3f0c18cfd4ca6b523d8c4c12
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-08-08 21:51:22 +0500

    mock search function for timing test

--- MyBot.cs
+++ MyBot.cs
@@ -5,9 +5,41 @@ public class MyBot : IChessBot
 {
     public bool DEBUG = false; // #DEBUG
     public Random rng = new();
+
+    public Timer timeControl;
+    public int MillisecondsAllocatedForSearch = 1000; 
+    public int maxDepth = 40;
+    public bool ContinueSearch = true;
+    public Move moveToPlay;
+
     public Move Think(Board board, Timer timer)
     {
+        ContinueSearch = true;
+        timeControl = timer;
+        int depth = 2;
+        while ( ContinueSearch || depth < maxDepth) {
+            moveToPlay = Search(board, depth);
+            depth += 2;
+        }
+        Console.WriteLine($"Final Depth: {depth}, Time it took: {timer.MillisecondsElapsedThisTurn}"); // #DEBUG
+        return moveToPlay;
+    }
+
+    Move Search(Board board, int depth) {  
         Move[] moves = board.GetLegalMoves();
+        
+        foreach (Move m in moves) {
+            if (timeControl.MillisecondsElapsedThisTurn > MillisecondsAllocatedForSearch) {
+                ContinueSearch = false;
+                break;
+            }
+            board.MakeMove(m);
+            System.Threading.Thread.Sleep(50);          // #DEBUG
+            Search(board, depth - 1);
+            board.UndoMove(m);
+
+        }
+
         return moves[rng.Next(moves.Length)];
     }
 }
