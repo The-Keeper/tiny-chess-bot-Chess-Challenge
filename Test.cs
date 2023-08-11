6001c82f7b4acd58cb4160862f2f422dd67bb91e
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-08-12 00:25:48 +0500

    test settings

--- Test.cs
+++ Test.cs
@@ -7,6 +7,7 @@ using System.Linq;
 
 Test.Run(); // comment this line out to run the challenge program itself
 
+
 public static class Test {
     public static void Run()
     {
@@ -16,11 +17,13 @@ public static class Test {
         (int numTokens, int debugCount) = ChessChallenge.Application.TokenCounter.CountTokens(txt);
         Console.WriteLine($"Bot Brain Capacity: {numTokens - debugCount}");
 
-        Timer timer = new Timer(60000);
         MyBot bot = new();
+        bot.DEBUG = true;
+        bot.MillisecondsAllocatedForSearch = 5000;
 
         // Board board = Board.CreateBoardFromFEN("4k3/8/8/8/8/8/P3P3/4K3 w - - 0 1");                             // king and two pawns
         Board board = Board.CreateBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");     // starting position
+        Timer timer = new Timer(60000);
         Move move = bot.Think(board, timer);
         Console.WriteLine(move);
         
